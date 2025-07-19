c8a8a28b0fd3bd9c6d7643b1dbda335f90d0e8ef
tidal-t <153453762+tidal-t@users.noreply.github.com>
2025-07-19 16:24:44 +0330

    Add files via upload

--- src/components/Drawer/components/Footer/Footer.jsx
+++ src/components/Drawer/components/Footer/Footer.jsx
@@ -5,18 +5,22 @@ export default function Footer() {
         <footer className='main_footer'>
             <div className="footer_wrapper">
                 <div className='footer_content'>
-                    <div>
+                    <section>
                         <div className="logo_wrapper">
                             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" viewBox="0 0 512 512"><path fill="currentColor" d="M126.12 315.1A47.06 47.06 0 1 1 79.06 268h47.06Zm23.72 0a47.06 47.06 0 0 1 94.12 0v117.84a47.06 47.06 0 1 1-94.12 0Zm47.06-188.98A47.06 47.06 0 1 1 244 79.06v47.06Zm0 23.72a47.06 47.06 0 0 1 0 94.12H79.06a47.06 47.06 0 0 1 0-94.12Zm188.98 47.06a47.06 47.06 0 1 1 47.06 47.1h-47.06Zm-23.72 0a47.06 47.06 0 0 1-94.12 0V79.06a47.06 47.06 0 1 1 94.12 0ZM315.1 385.88a47.06 47.06 0 1 1-47.1 47.06v-47.06Zm0-23.72a47.06 47.06 0 0 1 0-94.12h117.84a47.06 47.06 0 1 1 0 94.12Z" /></svg>
                         </div>
-                    </div>
-                    <div>
+                    </section>
+                    <section >
                         <ul className="footer_list">
                             <li><a href="/movies">movies</a></li>
                             <li><a href="/trending">trending</a></li>
                         </ul>
-                    </div>
-                    <div className="footer_email">darvishitaha@gmail.com</div>
+                    </section>
+                    <section style={{ textAlign: "end" }}>
+                        <a className="footer_tel btn-base" href="https://t.me/tidal_now">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256"><path d="M228.88,26.19a9,9,0,0,0-9.16-1.57L17.06,103.93a14.22,14.22,0,0,0,2.43,27.21L72,141.45V200a15.92,15.92,0,0,0,10,14.83,15.91,15.91,0,0,0,17.51-3.73l25.32-26.26L165,220a15.88,15.88,0,0,0,10.51,4,16.3,16.3,0,0,0,5-.79,15.85,15.85,0,0,0,10.67-11.63L231.77,35A9,9,0,0,0,228.88,26.19Zm-61.14,36L78.15,126.35l-49.6-9.73ZM88,200V152.52l24.79,21.74Zm87.53,8L92.85,135.5l119-85.29Z"></path></svg>
+                        </a>
+                    </section>
                 </div>
                 <div className="line_footer"></div>
                 <div className="footer_copyright">
--- src/components/ItemCarousel/ItemCarousel.jsx
+++ src/components/ItemCarousel/ItemCarousel.jsx
@@ -1,8 +1,11 @@
-import "./carousel.css"
+
 import Item from '../Item';
 import ItemLoading from "../Loadings/ItemLoading";
-import { useState, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./carousel.css"
 const options = {
     method: 'GET',
     headers: {
@@ -12,31 +15,67 @@ const options = {
 };
 export default function ItemCarousel({ title, url }) {
     const [data, setData] = useState();
-    const navigate = useNavigate();
-    const location = useLocation();
-    const type = data && (data[0].first_air_date != undefined ? "tv" : "movie");
-    const handleClick = (item) => {
-        navigate(`/${type}/${item.id}`, {
-            state: { backgroundLocation: location },
-        });
+    const sliderRef = useRef(null);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const settings = {
+        infinite: false,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        swipeToSlide: true,
+        afterChange: (index) => setCurrentIndex(index),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+        ]
     };
+    useEffect(() => {
+        console.log(currentIndex)
+    }, [currentIndex])
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/${url}`, options)
             .then(res => res.json())
-            .then(res =>setData(res.results))
+            .then(res => setData(res.results))
             .catch(err => console.error(err));
     }, [])
-    const List = data && data.map(item => { return (<Item key={item.id} type={item.first_air_date ? "tv" : "movie"} onClick={() => handleClick(item)} data={item}/>) })
+
+    const List = data && data.map(item => { return (<Item key={item.id} type={item.first_air_date ? "tv" : "movie"} data={item} />) })
     return (
         <div className="carousel">
             <div className="carousel__header">
-                <p className="header--title">{title}</p>
-                <button className="btn-base header--more">Explore More</button>
+                <div className="header_control">
+                    <p className="header--title">{title}</p>
+                    <button className="btn-base header--more">Explore More</button>
+                </div>
+                <div className="header_carousel_control">
+                    <button className="btn-base btn-header btn-control" onClick={() => sliderRef.current.slickPrev()}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 256 256"><path d="M165.66,202.34a8,8,0,0,1-11.32,11.32l-80-80a8,8,0,0,1,0-11.32l80-80a8,8,0,0,1,11.32,11.32L91.31,128Z"></path></svg>
+                    </button>
+                    <button className="btn-base btn-header btn-control" onClick={() => sliderRef.current.slickNext()}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 256 256"><path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path></svg>
+                    </button>
+                </div>
             </div>
             <div className="carousel__nav">
-                <div className="nav--wrapper">
+                <Slider ref={sliderRef} {...settings}>
                     {List ? List : <ItemLoading />}
-                </div>
+                </Slider>
             </div>
         </div>
     )
